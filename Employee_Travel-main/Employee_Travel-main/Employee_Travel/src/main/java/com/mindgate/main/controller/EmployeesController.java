6d1421bf88d862cb4632bdc2c1d6e8dd6b5fe108
Sibi5 <116270220+Sibi5@users.noreply.github.com>
2023-11-05 11:19:03 +0530

    Add files via upload

--- Employee_Travel-main/Employee_Travel-main/Employee_Travel/src/main/java/com/mindgate/main/controller/EmployeesController.java
+++ Employee_Travel-main/Employee_Travel-main/Employee_Travel/src/main/java/com/mindgate/main/controller/EmployeesController.java
@@ -4,6 +4,7 @@ package com.mindgate.main.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +19,33 @@ public class EmployeesController {
 	EmployeesServiceInterface employeesServiceInterface;
 	
 	// GET ALL
-			// http://localhost:8081/employees
-			@RequestMapping(value = "employees", method = RequestMethod.GET)
+			// http://localhost:8081/allemployees
+	
+			@RequestMapping(value = "allemployees", method = RequestMethod.GET)
 			public List<Employees> getAllEmployees() {
 				List<Employees> list = employeesServiceInterface.getAllEmployees();
 				return list;
 			}
-
+			
+	// GET BY ID
+			// http://localhost:8081/allemployees/{employeeId}
+				
+				@RequestMapping(value="allemployees/{employeeId}")
+				public Employees getByEmployeeId(@PathVariable int employeeId) {
+					return employeesServiceInterface.getEmployeeByEmployeeId(employeeId);
+				}
+				
+	// DELETE BY ID
+			// http://localhost:8081/deleterequest/{employeeId}
+				
+				@RequestMapping(value = "eleterequest/{employeeId}", method = RequestMethod.DELETE)
+				public boolean deleteEmployeeById(@PathVariable int employeeId) {
+					return employeesServiceInterface.deleteEmployeeByEmployeeId(employeeId);
+				}
+				
+	
+				
+	
+				
+	
 }
